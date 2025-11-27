a8e6d8acd6c65bf041986a9cdeee8abcc7b34fd1
ibrahim elsheikh <ielsheikh976@gmail.com>
2025-11-27 22:54:35 +0200

    first commit

---
+++ app/Projects/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+
+
+function Project({ params }: { params: { id: string } }) {
+    return (
+        <div>
+            Project{params.id}
+        </div>
+    );
+}
+
+export default Project;
